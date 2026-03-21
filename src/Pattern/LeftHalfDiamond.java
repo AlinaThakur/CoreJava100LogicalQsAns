955eda37bf3eaca334b02e9e637585a2d7c86143
Pooja Thakur <poojacodecraft@gmail.com>
2026-03-21 13:59:56 +0530

    RightHalfPyramid,ReverseRightHalfPyramid,RightHalfDiamond And Vice varsa of all

---
+++ src/Pattern/LeftHalfDiamond.java
@@ -0,0 +1,32 @@
+package Pattern;
+
+public class LeftHalfDiamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		left upper half pyramid
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+//		left lower half pyramid
+		for(int i=n-1;i>=1;i--) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
