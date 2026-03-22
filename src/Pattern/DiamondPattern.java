f8c7da5651bc3245a75635b8558b54eab26b14ed
Pooja Thakur <poojacodecraft@gmail.com>
2026-03-22 16:23:48 +0530

    DiamondStar, ReverseTriangle, Rhombus, SquareFillPattern etc

---
+++ src/Pattern/DiamondPattern.java
@@ -0,0 +1,29 @@
+package Pattern;
+
+public class DiamondPattern {
+
+	public static void main(String[] args) {	
+//		Upper Half Diamond
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+//		lower Half Diamond
+		for(int i=n-1;i>=1;i--) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*" +" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
